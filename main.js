1fe677f43df6b0caf33c825a73f8d38d1e81429a
krishpranav <krisna.pranav@gmail.com>
2021-03-18 15:38:22 +0530

    done changes

---
+++ main.js
@@ -0,0 +1,10 @@
+var main_SCANURLS=null;
+var main_fulesloaded=0;
+var main_files2load=5;
+
+function _run(){
+	loadjs("images.js");
+	loadjs("scanlist.js");
+	loadjs("settings.js");
+	loadjs("target.js")
+}
